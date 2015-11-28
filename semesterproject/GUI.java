ee4695f521b757847c88162039ba3fe3dc8000fd
Tanja <Tanja@tanjas-air>
2015-11-28 19:49:41 +0100

    nu med funktionel rabat kode courtesy philip

--- semesterproject/GUI.java
+++ semesterproject/GUI.java
@@ -19,13 +19,13 @@ import javax.swing.JOptionPane;
  *
  * @author Dino
  */
-public class Test extends javax.swing.JFrame {
+public class GUI extends javax.swing.JFrame {
     private Hall hall;
     private TicketHandler tktHandler;
     /**
      * Creates new form Test
      */
-    public Test() {
+    public GUI() {
         hall = new Hall(14, 20);
         tktHandler = new TicketHandler();
         initComponents();
@@ -736,20 +736,21 @@ public class Test extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(Test.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(Test.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(Test.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(Test.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(GUI.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new Test().setVisible(true);
+                new GUI().setVisible(true);
             }
         });
     }
